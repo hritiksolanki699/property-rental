24f57a05d4418993564116a2e3e513f6a797940f
Hritik <hritiksolanki699@gmail.com>
2024-03-31 20:22:42 +0530

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+import Footer from '@/components/Footer';
+import Hero from '../components/Hero';
+import InfoBoxes from '@/components/InfoBoxes';
 
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <Footer />
+    </>
+  );
+};
 export default HomePage;
