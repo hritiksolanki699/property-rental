43ce0c21ab15283ede6a100be3bcbb8cca8de24d
Hritik <hritiksolanki699@gmail.com>
2024-04-04 23:03:24 +0530

    property detail page

--- utils/requests.js
+++ utils/requests.js
@@ -1,5 +1,6 @@
 const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
 
+// fetch all properties
 async function fetchProperties() {
   try {
     //handle the case where the domain is not available yet.
@@ -18,4 +19,25 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+// Fetch a single property
+async function fetchProperty(id) {
+  try {
+    // Handle the case when the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching property:", error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
