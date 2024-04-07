48d67b15337608fb6a1878a0b3111b007d8f2f1c
Hritik <hritiksolanki699@gmail.com>
2024-04-07 22:44:49 +0530

    next google auth complete

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import Navbar from "@/components/Navbar";
 import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "PropertyPulse | Find The Perfect Rental",
@@ -9,12 +11,15 @@ export const metadata = {
 
 const layout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
